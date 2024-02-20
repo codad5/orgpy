78bd175fd5cb547f7566231847954296be5651aa
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-02-20 18:35:30 +0100

    added sort by exr functionality

--- main.py
+++ main.py
@@ -5,23 +5,71 @@ from utils import helper
 
 app = typer.Typer()
 
-ext_to_path_mapping = {}
+
+# sortby would be either "ext" or size
 
 
 @app.command()
-def hello(dir: str):
+def start(dir: str, sortby: str = "ext"):
+    if not helper.is_dir_exists(dir):
+        typer.echo(f"Directory {dir} does not exist")
+        return
     typer.echo(f"Hello {dir}")
+    if sortby == "ext":
+        sort_by_ext(dir)
+    else:
+        sort_by_size(dir)
+
+
+def sort_by_ext(dir: str):
     extensions = helper.get_all_existing_ext_in_dir(dir)
     questions = []
     for ext in extensions:
+        if ext == "":
+            continue
         question = {
             'type': 'input',
             'name': ext,
             'message': "Where do you want to store all your " + ext + " files"
         }
         questions.append(question)
-    answer = prompt(questions)
-    print_json(answer)
+    answer = ask_questions(questions)
+    print(answer)
+    helper.move_by_ext(dir, answer)
+
+
+def ask_questions(questions, include_default=True):
+    if include_default:
+        questions.append({
+            "type": "input",
+            "name": "default",
+            "message": "Enter the default directory",
+            "default": "."
+        })
+    answers = {
+        "default": ""
+    }
+    for question in questions:
+        answer = prompt(question)
+        if "--end" in answer.values():
+            default = ask_questions([{
+                "type": "input",
+                "name": "default",
+                "message": "Enter the default directory",
+                "default": "."
+            }], False)
+            default = default["default"]
+            answer = {"default": default}
+            answers.update(answer)
+            break
+        elif "--pass" in answer.values():
+            continue
+        answers.update(answer)
+    return answers
+
+
+def sort_by_size(dir):
+    pass
 
 
 if __name__ == "__main__":
--- utils/helper.py
+++ utils/helper.py
@@ -22,3 +22,30 @@ def get_all_existing_ext_in_dir(path=".", recursive=False):
         file = Path(os.path.join(path, i))
         extensions.add(file.suffix)
     return extensions
+
+
+def is_dir_exists(dir) -> bool:
+    return os.path.isdir(dir)
+
+
+def move_by_ext(path, ext_to_path_mapping=None):
+    if ext_to_path_mapping is None:
+        ext_to_path_mapping = dict()
+    files = get_all_file_recursively(path)
+    for file in files:
+        file = Path(file)
+        if file.suffix in ext_to_path_mapping:
+            src = os.path.join(path, file)
+            # move to the new directory created inside the src directory
+            dest = os.path.join(path, ext_to_path_mapping[file.suffix])
+        else:
+            default = ext_to_path_mapping["default"]
+            src = os.path.join(path, file)
+            dest = os.path.join(path, default)
+
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+        os.rename(src, os.path.join(dest, file))
+        print(f"Moved {src} to {dest}")
+    print("All files moved successfully")
+
